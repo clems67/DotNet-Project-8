5ea90e6c51eb3ae6242d3a9b3032bcdf537e3a9a
clems67 <caravita.clement.67150@gmail.com>
2024-10-04 17:27:43 +0200

    refacto snack bar and status

--- CalifornianHealthMonolithic/Controllers/BookingController.cs
+++ CalifornianHealthMonolithic/Controllers/BookingController.cs
@@ -1,5 +1,7 @@
 ﻿using CalifornianHealthMonolithic.Code;
 using CalifornianHealthMonolithic.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
@@ -9,43 +11,29 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Security.Cryptography.Xml;
 using System.Text;
+using Microsoft.AspNetCore.Mvc;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Web;
 using System.Web.Http;
+using System.Web.Http.Results;
 using System.Web.Mvc;
 
 namespace CalifornianHealthMonolithic.Controllers
 {
-    public class CreateAppointmentDto
-    {
-        public int consultantId { get; set; }
-        public string patientName { get; set; }
-    }
     public class BookingController : Controller
     {
         public RpcClient rpcClient;
-        [System.Web.Mvc.HttpPost]
-        public string TestPostMethod(CreateAppointmentDto createAppointmentDto)
-        {
-            Debug.WriteLine($"\nTestMehod consultantId: {createAppointmentDto.consultantId}\n");
-            Debug.WriteLine("conflict");
-            return "409";
-        }
-        [System.Web.Mvc.HttpGet]
-        public string TestGetMethod(int id)
-        {
-            var returnedValue = new CreateAppointmentDto() { consultantId = 1, patientName = "bob" };
-            return JsonConvert.SerializeObject(returnedValue);
-        }
+
         [System.Web.Mvc.HttpGet]
         public async Task<ActionResult> GetConsultantCalendar()
         {
             HttpClient httpClient = new HttpClient();
             var responseContent = await (await httpClient.GetAsync("http://localhost:55258/Home/GetConsultants")).Content.ReadAsStringAsync();
-            var fagr = "";
             var communicationModel = JsonConvert.DeserializeObject<List<Shared.ConsultantModel>>(responseContent);
 
             var consultants = new List<Models.ConsultantModel>();
@@ -66,6 +54,7 @@ namespace CalifornianHealthMonolithic.Controllers
             };
             return View(consultantModelList);
         }
+
         [System.Web.Mvc.HttpGet]
         public async Task<string> GetConsultantCalendarRequest(int id)
         {
@@ -78,21 +67,11 @@ namespace CalifornianHealthMonolithic.Controllers
                 }
             );
 
-            var timeoutCounter = 0;
-            while (rpcClient.communicationModel == null)
-            {
-                timeoutCounter += 1;
-                Thread.Sleep(100);
-                if (timeoutCounter == 150)
-                {
-                    return "503"; //Service Unavailable error
-                }
-            }
             return JsonConvert.SerializeObject(rpcClient.communicationModel.Appointments);
         }
 
         [System.Web.Mvc.HttpPost]
-        public async Task<string> BookAppointment([FromBody] AppointmentModel appointmentModel)
+        public async Task<ActionResult> BookAppointment([FromBody] AppointmentModel appointmentModel)
         {
             var communicationModel = new CommunicationModel
             {
@@ -101,21 +80,12 @@ namespace CalifornianHealthMonolithic.Controllers
             };
             var response = await rpcClient.CallAsync(communicationModel);
 
-            var timeoutCounter = 0;
-            while (rpcClient.communicationModel == null)
-            {
-                timeoutCounter += 1;
-                Thread.Sleep(100);
-                if (timeoutCounter == 150)
-                {
-                    return "503"; //Service Unavailable error
-                }
-            }
             if (rpcClient != null && rpcClient.communicationModel != null)
             {
-                return rpcClient.communicationModel.IsAppointmentsCreated ? "200" : "409"; //OK or Conflict
+                int statusCode = rpcClient.communicationModel.IsAppointmentsCreated ? 200 : 409;
+                return new HttpStatusCodeResult(statusCode);
             }
-            return "500";
+            return new HttpStatusCodeResult(500);
         }
     }
 }
